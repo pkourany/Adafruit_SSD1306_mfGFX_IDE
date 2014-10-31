7eb88d3f56c37733b611f3b712a8ae1cbfd7d2cd
pkourany <pkourany@gmail.com>
2014-10-31 14:08:37 -0400

    First commit

---
+++ firmware/examples/ssd1306_user.h
@@ -0,0 +1,6 @@
+#define SSD1306_I2C_ADDRESS   0x3C	// 011110+SA0+RW - 0x3C or 0x3D
+// Address for 128x32 is 0x3C
+// Address for 128x64 is 0x3D (default) or 0x3C (if SA0 is grounded)
+
+#define SSD1306_128_64
+//#define SSD1306_128_32
